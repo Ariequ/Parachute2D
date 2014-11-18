283463bef7c702e808db8eb1329c9eb071d8da71
Ariequ <shangxxia@hotmail.com>
2014-11-19 00:23:34 +0800

    add coin effect audio

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject effect;
     Animator animator;
 
     // Use this for initialization
@@ -16,10 +17,19 @@ public class CoinController : MonoBehaviour
     {
         animator.speed = 1;
         animator.SetBool ("MeetPilot", true);
+        initEffect(effect);
+        audio.Play();
     }
 
     public void OnAnimationEnd()
     {
         Destroy (gameObject);
+    }
+
+    //Spawn effect method
+    private void initEffect(GameObject prefab){
+        GameObject go = (GameObject) Instantiate(prefab, transform.position, Quaternion.identity);
+        go.transform.parent = transform;
+        go.transform.localPosition = new Vector3(go.transform.localPosition.x, go.transform.localPosition.y, go.transform.localPosition.z);    
     }
 }
