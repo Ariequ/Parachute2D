6da2b97c294ffc81724e5eb10718cb212f6ecf29
Ariequ <shangxxia@hotmail.com>
2014-12-06 23:25:29 +0800

    ad activity

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,6 +5,7 @@ public class StartGame : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
         Application.LoadLevel(1);
 	}
 }
