0e19ae0bb63ae3559073726b6e299711d48d1296
Ariequ <shangxxia@hotmail.com>
2014-12-23 14:05:17 +0800

    更新关卡

---
+++ Assets/Scripts/NotificationCenter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotificationCenter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_ANDROID
+        AndroidJavaObject nativeObj = new AndroidJavaObject("AlarmReceiver");  
+        nativeObj.CallStatic("startAlarm", new object[5]{"title","title", "msg", 10, 1});  
+#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
