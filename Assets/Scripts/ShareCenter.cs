3699f2af498bab232ccf848597663f0d028e0ecc
Ariequ <shangxxia@hotmail.com>
2015-01-01 00:34:13 +0800

    iPad 布局 分享内容。
    iPad 布局 分享内容。

--- Assets/Scripts/ShareCenter.cs
+++ Assets/Scripts/ShareCenter.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO;
+using System;
 
 public class ShareCenter : MonoBehaviour {
 
     //分享本地纹理
     public Texture2D texture;
+    public StartUIController startUIController;
 	void Start () {
 #if UNITY_EDITOR
 		this.enabled = false;
@@ -35,7 +37,7 @@ public class ShareCenter : MonoBehaviour {
         Social.SetWechatAppId("wx6dc874911aca6ad5");
         
 //        Social.SetTargetUrl("http://a.app.qq.com/o/simple.jsp?pkgname=com.ariequ.parachute");
-        Social.SetTargetUrl("http://ariequ.github.io/blog/2014/12/17/parachute-page/");
+        Social.SetTargetUrl("https://itunes.apple.com/cn/app/yi-lu-dao-di/id936408830?l=zh&ls=1&mt=8");
         
         //设置分享回调
         //如果无需回调 OpenShareWithImagePath最后一个参数可不填
@@ -56,7 +58,8 @@ public class ShareCenter : MonoBehaviour {
         //纹理 需要为 ARGB32 and RGB24 格式. 而且纹理的import settings/Advanced下的"Read/Write Enabled"需要勾选
         File.WriteAllBytes(Application.persistentDataPath + "/icon.png",texture.EncodeToPNG());
         //打开分享面版
-        Social.OpenShareWithImagePath(platforms, "我在#Fall Down 100#玩的正爽，你也来试试吧！", Application.persistentDataPath + "/icon.png", callback);
+        string shareString = String.Format("我在#一撸到底#撸了{0}分，来试试吧,iOS点击图片下载", startUIController.CurrentScore.ToString());
+        Social.OpenShareWithImagePath(platforms, shareString, Application.persistentDataPath + "/icon.png", callback);
 #endif
     }
 }
