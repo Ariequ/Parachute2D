4d0eec2557f656e4b903fb956423e04158f4a01b
Ariequ <shangxxia@hotmail.com>
2015-10-31 16:24:23 +0800

    add ci support

---
+++ Assets/Editor/BuildPackage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class BuildPackage : MonoBehaviour
+{
+	[MenuItem("Tools/Build")]
+	public static void Build ()
+	{
+		Debug.Log (Application.dataPath + "/build");
+		BuildPipeline.BuildPlayer(GetBuildScenes(), Application.dataPath + "/../build", BuildTarget.iOS, BuildOptions.None); 
+	}
+
+	static string[] GetBuildScenes()
+	{ 
+		List<string> names = new List<string>(); 
+		
+		foreach (EditorBuildSettingsScene e in EditorBuildSettings.scenes)
+		{ 
+			if (e == null) 
+				continue; 
+			
+			if (e.enabled)
+			{
+				Debug.Log("======= EditorBuildSettingsScene =========" + e.path);
+				names.Add(e.path); 
+			}
+		} 
+		return names.ToArray(); 
+	}
+}
