fdfadb87704149844afd3c8b7ab792a798408a14
Ariequ <shangxxia@hotmail.com>
2015-11-01 18:09:35 +0800

    ADD BUILD CONFIG

--- Assets/Editor/BuildPackage.cs
+++ Assets/Editor/BuildPackage.cs
@@ -5,16 +5,24 @@ using System.Collections.Generic;
 
 public class BuildPackage : MonoBehaviour
 {
+    private static string locationPathName = Application.dataPath + "/../build/";
+    private static BuildTarget buildTarget = BuildTarget.iOS;
+    private static bool ONLY_RESOURCE = false;
+
 	[MenuItem("Tools/Build")]
 	public static void Build ()
 	{
+        ConfigArg();
 		Debug.Log (Application.dataPath + "/build");
 		BuildPipeline.BuildPlayer(GetBuildScenes(), Application.dataPath + "/../build", BuildTarget.iOS, BuildOptions.None); 
 	}
 
-    [MenuItem("Tools/Build")]
+    [MenuItem("Tools/BuildAndroid")]
     public static void BuildAndroid ()
-    {        
+    {      
+        ConfigArg();
+        PlayerSettings.Android.keyaliasPass = "%TGB6yhn";
+        PlayerSettings.Android.keystorePass = "%TGB6yhn";
         BuildPipeline.BuildPlayer(GetBuildScenes(), Application.dataPath + "/../build/parachute.apk", BuildTarget.Android, BuildOptions.None); 
     }
 
@@ -35,4 +43,39 @@ public class BuildPackage : MonoBehaviour
 		} 
 		return names.ToArray(); 
 	}
+
+    private static void ConfigArg()
+    {
+        //      buildSettingPath = BUILD_SETTING_PATH;
+        foreach (string arg in System.Environment.GetCommandLineArgs())
+        {
+            if (arg.Contains("iPhone"))
+            {
+                buildTarget = BuildTarget.iOS;
+            }
+            else if (arg.Contains("Android"))
+            {
+                buildTarget = BuildTarget.Android;
+                //              buildSettingPath = BUILD_SETTING_PATH_ANDROID;
+            }        
+            else if (arg.Contains("bundleVersion"))
+            {
+                PlayerSettings.bundleVersion = arg.Substring(arg.IndexOf("=") + 1);
+
+                Debug.Log("========set version ========= " + arg.Substring(arg.IndexOf("=") + 1));
+            }
+            else if(arg.Contains("versionCode"))
+            {
+                PlayerSettings.Android.bundleVersionCode = int.Parse(arg.Substring(arg.IndexOf("=") + 1));
+                Debug.Log("========set bundleVersionCode ========= " + arg.Substring(arg.IndexOf("=") + 1));
+            }
+            else if(arg.Contains("BundleID"))
+            {
+                PlayerSettings.bundleIdentifier = arg.Substring(arg.IndexOf("=") + 1);
+                Debug.Log("========set BundleID ========= " + arg.Substring(arg.IndexOf("=") + 1));
+            }
+        }
+
+        PlayerPrefs.DeleteAll();
+    }
 }
